e6370a20d7725752175d2f30ef331ab6b5a4ff74
pkmnnerd <b_liu72@yahoo.com>
2022-05-23 20:10:25 +0000

    Initial commit

---
+++ src/main/java/com/jumpydoll/pkmnnerd/javatutorialproject/JavaTutorialProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jumpydoll.pkmnnerd.javatutorialproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTutorialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTutorialProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumpydoll/pkmnnerd/javatutorialproject/JavaTutorialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumpydoll.pkmnnerd.javatutorialproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTutorialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
